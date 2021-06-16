d90cfada71975dd9919ff5e01a8520eeb4a1d4a9
jonathon hawkins <jonathonhawkins92@gmail.com>
2021-06-16 18:59:03 +0100

    adding annimaitons

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PokemonOverview } from '../../types/pokemon';
+	import { fade, scale } from 'svelte/transition';
 
 	export let name: PokemonOverview['name'];
 	export let id: PokemonOverview['id'];
@@ -9,7 +10,8 @@
 <a
 	class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
 	href={`/pokemon/${id}`}
+	transition:fade
 >
-	<img class="h-40 w-40" src={image} alt={name} />
+	<img class="h-40 w-40" src={image} alt={name} transition:scale />
 	<h2 class="capitalize text-2xl">{id}: {name}</h2>
 </a>
